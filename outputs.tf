86eb5b2766cec3df3eeb3f6fbf7194fe4e058755
saumitra-dev <saumitra@infraspec.dev>
2024-07-16 12:13:37 +0530

    refactor: export `cluster_name` attribute to promote better implicit dependability

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,13 @@
 ##############################
-# ECS Cluster Outputs
+# ECS Cluster
 ##############################
 
 output "cluster_arn" {
   description = "ARN of the ECS Cluster"
   value       = module.ecs_cluster.arn
+}
+
+output "cluster_name" {
+  description = "Name of the ECS Cluster"
+  value       = var.cluster_name
 }
